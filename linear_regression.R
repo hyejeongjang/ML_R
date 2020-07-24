017fafdcf842fba7c0c03080a8afd9cd905d87b0
hyejeongjang <hj970219@naver.com>
2020-07-25 05:55:53 +0900

    선형회귀 수정

---
+++ linear_regression.R
@@ -0,0 +1,19 @@
+### 데이터 불러오기
+library(MASS)
+data=Boston
+
+### 단순선형회귀
+lm.bh=lm(medv~., data=data)
+summary(lm.bh)
+anova(lm.bh)
+
+### PLOT
+layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page 
+plot(lm.bh)
+
+### 로지스틱회귀모형
+library(boot)
+data(nodal)
+rd = nodal[,-1]
+gfit = glm(r~., data=rd, family="binomial")
+summary(gfit)
